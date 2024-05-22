d96d0671c70d37615fb7638d5b3aeeb3ad1cf244
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-05-22 18:19:03 +0200

    Tasca 5 - Historia 4

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,51 +5,64 @@ import { useGlobalContext } from '../context/GlobalContext';
 export default function FormModal(){
     const { dataHistòria, setDataHistòria } = useGlobalContext()
 
-    function controladorFormHistòria() {
-        
+    function controladorFormHistòria(e) {
+        const { name, value } = e.target;
+        setDataHistòria(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
     }
 
     return (
         <ModalContent>
             {(onClose) => (
                 <>
-                    <ModalHeader className="flex flex-col gap-1">Editar Historia</ModalHeader>
+                    <ModalHeader className="flex flex-col gap-1">Editar Historia &quot;{dataHistòria ? dataHistòria.titulo : ''}&quot;</ModalHeader>
                     <ModalBody>
                         <Input
                             className='mb-3'
                             label="Fecha"
                             placeholder="Selecciona una fecha"
                             endContent={<Calendar className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />}
+                            name="fecha"
                             value={dataHistòria ? dataHistòria.fecha : ''}
-                            onChange={(e) =>}
+                            onChange={controladorFormHistòria}
                         />
                         <Input
                             className='mb-3'
                             label="Título"
                             placeholder="Introduce el título"
                             endContent={<Pencil className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />}
+                            name="titulo"
                             value={dataHistòria ? dataHistòria.titulo : ''}
+                            onChange={controladorFormHistòria}
                         />
                         <Textarea
                             className='mb-3'
                             label="Experiencia"
                             placeholder="Introduce la experiencia"
                             minRows={5}
+                            name="experiencia"
                             value={dataHistòria ? dataHistòria.experiencia : ''}
+                            onChange={controladorFormHistòria}
                         />
                         <Textarea
                             className='mb-3'
                             label="Comentario"
                             placeholder="Introduce un comentario"
                             minRows={5}
+                            name="comentario"
                             value={dataHistòria ? dataHistòria.comentario : ''}
+                            onChange={controladorFormHistòria}
                         />
                         <Input
                             className='mb-3'
                             label="Imagen"
                             placeholder="URL imagen"
                             endContent={<Image className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />}
+                            name="imagen"
                             value={dataHistòria ? dataHistòria.imagen : ''}
+                            onChange={controladorFormHistòria}
                         />
                     </ModalBody>
                     <ModalFooter>
