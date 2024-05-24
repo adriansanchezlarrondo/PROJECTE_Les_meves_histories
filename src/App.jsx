007309b747d03e630f6a78016f4ddbbb1d102494
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-05-24 18:19:04 +0200

    historia 5

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import FormModal from './components/Modal.jsx';
 import { useGlobalContext } from './context/GlobalContext.jsx';
 
 export default function App() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const { setDataHistòria } = useGlobalContext()
 
   function controladorEditarHistòria() {
